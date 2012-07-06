8f1e608b8eb6afdd877b5abd84b358039ad6b6c6
Joseph W. Breu <breu@breu.org>
2012-07-06 13:06:59 -0500

    add metadata

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+maintainer        "Rackspace Hosting, Inc."
+license           "Apache 2.0"
+description       "kong module"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+
+%w{ ubuntu fedora }.each do |os|
+  supports os
+end
+
+%w{osops-utils}.each do |dep|
+  depends dep
+end
