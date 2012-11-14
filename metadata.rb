a2f47d4dfd77d4c752c6c1919161678fcc8bb9c2
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-11-14 12:20:03 -0600

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Rackspace Hosting, Inc."
 license           "Apache 2.0"
 description       "kong module"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.6"
+version           "1.0.7"
 
 %w{ ubuntu fedora }.each do |os|
   supports os
