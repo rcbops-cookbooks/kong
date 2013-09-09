7e5d9c7f865f760e0fdfedfff94b7854eb22389e
rackerjoe <breu@breu.org>
2013-09-09 16:20:09 -0500

    Updating version # to 4.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Rackspace Hosting, Inc."
 license           "Apache 2.0"
 description       "kong module"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.14"
+version           "4.1.2"
 
 %w{ centos ubuntu }.each do |os|
   supports os
